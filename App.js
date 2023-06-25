e6661009d0460a6d67e999f81fa5c82005115f5f
Makayla Colonello <115374967+makcolonello@users.noreply.github.com>
2023-06-25 17:48:44 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import Products from './Components/products'
+
+function App() {
+  return (
+  <Products></Products>
+  );
+}
+
+export default App;
